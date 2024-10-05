09f644cf342fb88c576a602bcd36a8457ca42db7
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-10-05 16:26:08 +0300

    feat(chapter): finish chapter 4

---
+++ 4-understanding-ownership/practice/src/main.rs
@@ -0,0 +1,15 @@
+fn main_1() {
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    let num: &mut i32 = &mut v[2];
+    *num += 1;
+    // println!("Vector is now {:?}", v);
+    println!("Third element is {}", *num);
+    println!("Vector is now {:?}", v);
+}
+
+fn main() {
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    let num: &mut i32 = &mut v[2];
+    let num2: &i32 = &*num;
+    println!("{} {}", *num, *num2);
+}
