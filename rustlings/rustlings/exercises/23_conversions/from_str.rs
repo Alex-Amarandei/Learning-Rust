c7905dc8c737e0f90d2ce25319b3de20cab5e713
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-11-24 16:20:29 +0400

    feat(rustlings): finish rustlings

--- rustlings/rustlings/exercises/23_conversions/from_str.rs
+++ rustlings/rustlings/exercises/23_conversions/from_str.rs
@@ -41,7 +41,28 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split_iterator = s.split(",");
+
+        let (Some(name), Some(age), None) = (
+            split_iterator.next(),
+            split_iterator.next(),
+            split_iterator.next(),
+        ) else {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age = age.parse().map_err(ParsePersonError::ParseInt)?;
+
+        Ok(Self {
+            name: name.into(),
+            age,
+        })
+    }
 }
 
 fn main() {
